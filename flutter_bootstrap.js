51ca74688d40870e45bb090d77784d4f1efcbaa5
mrrern <mrrern@users.noreply.github.com>
2025-03-19 00:04:56 +0000

    deploy: d455336786ff357a70e55a4a78360e7ca0072175

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -40,6 +40,6 @@ _flutter.buildConfig = {"engineRevision":"50f226569f331edf2831a7543f55d953c3d506
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4016341415"
+    serviceWorkerVersion: "858971949"
   }
 });
