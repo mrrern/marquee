398cecb1ee6121e2214ee7e65c4a647bf374b2de
mrrern <mrrern@users.noreply.github.com>
2025-08-17 05:03:04 +0000

    deploy: 3864721b1385bc0bdbd921bb9cae25b9b07d54ce

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -34,10 +34,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"f85f6b62b6db2a8725cafa75360ec960d581a809","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f7f","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3188368744"
+    serviceWorkerVersion: "3174704998"
   }
 });
